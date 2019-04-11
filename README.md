af3a869bdaf74b0583e215bfd5f424ffc49205c3
MaksymY <maksym.yankivskyypro@gmail.com>
2019-04-11 22:55:21 +0200

    initialization on MVC

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PHP-Practice-
-this is place where i juste train my self whith new methode 
+this is place where i juste train my self whith new methode in this it's MVC.
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+//Definition de la page courante
+if(isset($_GET['page']) && !empty($_GET['page'])) {
+  $page = trim(strtolower($_GET['page']));
+} else {
+    $page = 'home';
+};
+
+//Array contenant tout les pages
+$allPages = scandir('controllers/');
+
+if (in_array($page.'_controller.php', $allPages)){
+
+  //inclure la page
+  include_once 'models/'.$page.'_model.php';
+  include_once 'controllers/'.$page.'_controller.php';
+  include_once 'views/'.$page.'_view.php';
+
+}else{
+  echo'Erreur 404';
+}
+
+
+?>
+
