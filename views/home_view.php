069ed2a4d1e1b860241812085ae5e196dcf4a2e4
MaksymY <maksym.yankivskyypro@gmail.com>
2019-04-12 22:51:11 +0200

    all creation on pages and js an style whith methode MVC

--- views/home_view.php
+++ views/home_view.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include_once 'views/includes/head.php' ?>
     <title><?= ucfirst($page) ?> - Mon site !</title>
+    <script defer src="assets/js/app.js"></script>
 </head>
 <body>
-  <header>
-    <ul>
-      <li><a href="index.php?page=contact">Accueil</a></li>
-      <li><a href="index.php?page=contact">Contact</a></li>
-    </ul>
-  </header>
-    <h1>Hello word !</h1>
-  <footer>
-    footer
-  </footer>
+    <?php include_once 'views/includes/header.php' ?>
+    
+      <h1>Hello word !</h1>
+
+      <img class="Rayan" src="assets/images/Bioshock.jpg" alt="" >
+
+    <?php include_once'views/includes/footer.php' ?>
+
 </body>
 </html>
